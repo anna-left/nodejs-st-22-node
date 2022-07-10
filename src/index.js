4acfb10bbf077a0a520017534b363c47ce88aad7
anna-left <anna_left@mail.ru>
2022-07-10 22:20:43 +0300

    feat: task 1.1 implemented

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-const process = require('process');
-const { Transform, pipeline } = require('node:stream');
-const { EOL } = require('os');
+"use strict";
 
-const reverseLine = () => {
-  const stdin = process.stdin;
-  const stdout = process.stdout;
+var _process = require("process");
 
+var _nodeStream = require("node:stream");
+
+var _os = require("os");
+
+var reverseLine = function reverseLine() {
+  var stdin = _process.stdin;
+  var stdout = _process.stdout;
   console.log('Please enter data');
-
-  const transformReverse = new Transform({
-    transform(chunk, _, cb) {
-      const trimChunk = chunk.toString().trim();
-      const reversedChunk = trimChunk.split("").reverse().join("");
-      this.push(reversedChunk + EOL);
-      this.push(EOL);
+  var transformReverse = new _nodeStream.Transform({
+    transform: function transform(chunk, _, cb) {
+      var reversedChunk = chunk.toString().trim().split("").reverse().join("");
+      this.push(reversedChunk + _os.EOL);
+      this.push(_os.EOL);
       cb();
     }
-  })
-  pipeline(
-    stdin,
-    transformReverse,
-    stdout,
-    error => {
-      console.log(`Error: ${error}`);
-    }
-  )
+  });
+  (0, _nodeStream.pipeline)(stdin, transformReverse, stdout, function (error) {
+    console.log("Error: ".concat(error));
+  });
 };
 
 reverseLine();
