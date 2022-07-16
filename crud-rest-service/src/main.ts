93048908604d058ffc6eb1591f6baf7022f92d7c
anna-left <anna_left@mail.ru>
2022-07-16 21:57:10 +0300

    feat: add pagination

--- crud-rest-service/src/main.ts
+++ crud-rest-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { IUser } from './users/entities/user.entity';
+
+export const usersDB: IUser[] = [];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // transform: true, //here is the "magic"
+    }),
+  );
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
