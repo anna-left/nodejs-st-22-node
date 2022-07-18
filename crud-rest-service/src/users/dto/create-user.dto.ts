bb2a70512109bed8760174a871d8ff0978f98822
anna-left <anna_left@mail.ru>
2022-07-18 19:03:45 +0300

    feat: make all fields required

--- crud-rest-service/src/users/dto/create-user.dto.ts
+++ crud-rest-service/src/users/dto/create-user.dto.ts
@@ -2,15 +2,24 @@ import { IsBoolean, IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
   readonly login: string;
+
   @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
+
   @IsString()
   readonly password: string;
+
   @IsNumber()
+  @IsNotEmpty()
   @Min(4)
   @Max(130)
   readonly age: number;
+
   @IsBoolean()
+  @IsNotEmpty()
   readonly isDeleted: boolean;
 }
