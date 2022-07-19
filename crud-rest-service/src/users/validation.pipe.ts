881c55a8edfea796fb687cf548e0d7637d368439
anna-left <anna_left@mail.ru>
2022-07-19 10:40:38 +0300

    feat: check the uniqueness of the login

--- crud-rest-service/src/users/validation.pipe.ts
+++ crud-rest-service/src/users/validation.pipe.ts
@@ -1,17 +1,9 @@
-import {
-  PipeTransform,
-  BadRequestException,
-  ArgumentMetadata,
-} from '@nestjs/common';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
 
 import { CreateUserDto, UserSchema } from './dto/create-user.dto';
 
 export class CreateUserValidatorPipe implements PipeTransform<CreateUserDto> {
-  public transform(
-    value: CreateUserDto,
-    metadata: ArgumentMetadata,
-  ): CreateUserDto {
-    console.log(metadata);
+  public transform(value: CreateUserDto): CreateUserDto {
     const result = UserSchema.validate(value);
     if (result.error) {
       const errorMessages = result.error.details.map((d) => d.message).join();
